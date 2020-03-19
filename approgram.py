c9f37e01d31ac63437ef826633acabde537c0534
atulpandey63 <atulpandey8175@gmail.com>
2020-03-19 22:03:55 +0530

    example of python program to input by user

---
+++ approgram.py
@@ -0,0 +1,13 @@
+name= input('What is your name? ')
+print("My name is "+name)
+birth_year = input('Your Birth year? ')
+age=2020-int(birth_year)
+print(age)
+
+course='Python is best for beginners'
+print(course.upper())
+print(course.lower())
+print(course.replace('beginners',' Beginners '))
+print(len(course))
+print('Pythone'in course)
+print(abs(-56))
